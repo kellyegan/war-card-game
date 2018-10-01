f9c9205859b3a73fe3640f6ebc81e1e789e08588
kellyegan <kelly.egan@gmail.com>
2018-10-01 15:44:19 -0400

    Create Deck and Card objects

--- war.js
+++ war.js
@@ -1,5 +1,25 @@
 function War() {
+	this.deck = new Deck();
+}
 
+function Deck() {
+	let suits = ["Clubs","Diamonds","Hearts","Spades"];
+	let ranks = ["Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace"];
+
+	this.cards = [];
+	let count = 0;
+
+	suits.forEach((suit) => {
+		ranks.forEach((rank, index) => {
+			this.cards[count] = new Card( rank + " of " + suit, index);
+			count++;
+		});
+	});
+}
+
+function Card(name, value) {
+	this.name = name;
+	this.value = value;
 }
 
 module.exports = War;
