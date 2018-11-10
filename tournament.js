37cbcaae580766fb6df4d20890210a868a7bfdf8
kellyegan <kelly.egan@gmail.com>
2018-11-09 21:58:17 -0500

    Add tournament.js stub

---
+++ tournament.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ *  Single-elimination tournament, takes players with rank and number of tournament slots
+ */
+function Tournament( players, slots ) {
+	//
+	
+}
+
+
+
+module.exports = Tournament;
