5a8cca6763215fcf4670857aeb5237b1ba55e640
kellyegan <kelly.egan@gmail.com>
2018-11-12 21:41:49 -0500

    Add War to tournament. Save game results

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const Tournament = require('./tournament.js');
 const season = new RoundRobin(32);
 season.play();
 
-const postSeasonPlayers = season.players.slice(0, 5);
+const postSeasonPlayers = season.players.slice(0, 16);
 
 const tournament = new Tournament(postSeasonPlayers);
-
+tournament.play();
 
 // console.log( season.schedule.filter(game => game.winner === 9).length )
--- tournament.js
+++ tournament.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const War = require('./war.js');
+
 /**
  *  Single-elimination tournament, ranked list of players and number of tournament slots
  *
@@ -10,15 +12,63 @@ function Tournament( players ) {
 	const slots = smallestPowerOf(2, players.length);
 	this.players = players;
 
+	//Fill in empty slots until you reach next power of two
 	while(this.players.length < slots) {
 		this.players.push(null);
 	}
-	console.log(this.players)
 }
 
 Tournament.prototype.play = function () {
-	const sorted = this.players.sort( (a, b) => {return b.rank - a.rank});
-	console.log(sorted);
+	let remainingPlayers = this.players.map( (player, index) => {
+		return player ? player.id : null;
+	});
+	console.log(remainingPlayers);
+
+	let round = 0;
+	while( remainingPlayers.length > 1) {
+		let winners = [];
+
+		for( let i = 0; i < remainingPlayers.length / 2; i++){
+			let playerOneID = remainingPlayers[i];
+			let playerTwoID = remainingPlayers[remainingPlayers.length - (1 + i)];
+
+			if( playerOneID !== null ){
+				if( playerTwoID !== null ){
+					console.log(`Round ${round}: ${playerOneID} vs. ${playerTwoID}`);
+
+					const game = new War(2, 1);
+					game.deal();
+					const winner = game.play() == 0 ? playerOneID : playerTwoID;
+
+					const gameID = this.games.length;
+
+					this.games.push({
+						id: gameID,
+						round: round,
+						player1: playerOneID,
+						player2: playerTwoID,
+						winner: winner,
+						// hands: game.hands
+					});
+
+					winners.push(winner);
+				} else {
+					//No match this round. playerOne goes to next round
+					winners.push(playerOneID);
+				}
+			} else if( playerTwoID !== null ){
+				//No match this round. playerTwo goes to next round
+				winners.push(playerTwoID);
+			} else {
+				//Hmm both players are null?
+			}
+		}
+		round++;
+		remainingPlayers = winners;
+		console.log(remainingPlayers);
+	}
+	console.log(`Champion: ${remainingPlayers[0]}`);
+	console.log(this.games);
 }
 
 /**
