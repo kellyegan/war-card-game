1bf7c58770b946ba64807df8f10e0dde2899d53d
Kelly Egan <kelly.egan@gmail.com>
2022-11-18 14:31:04 -0500

    Change out for for forEach with parseHand method to start.

--- PlayByPlay.js
+++ PlayByPlay.js
@@ -102,66 +102,20 @@ class PlayByPlay {
         let calls = []
         let wordCount = 0;
 
-
         let spreadAtLastUpdate = 0;
         let streak = 0;
         let lastWinner = null;
         let leader = null;
         let lastLeader = null;
-
     
         let call = `Looks like ${this.players[0].fullName} and ${this.players[1].fullName} are ready to start.`;
         calls.push(call);
-    
-        for(let i = 0; i < this.hands.length; i++) {
-            let hand = this.hands[i];
-            call = ""
-            
-            if( hand.winners.length == 1) {
-                let winner = hand.winners[0];
-                let loser = (hand.winners[0] + 1) % 2;
+        
+        this.hands.forEach((hand, index) => {
+            let handDetails = this.parseHand(hand);
+            console.log(handDetails)
+        });
 
-                streak = lastWinner === winner ? streak + 1 : 0;
-                lastWinner = winner;
-
-                leader = hand.counts[0] > hand.counts[1] ? 0 : 1;
-                
-                if(leader != lastLeader) {
-                    call += `${this.players[winner].fullName} takes the lead with a ${this.deck.getName(hand.play[winner].identifier)}`;
-                } else if( hand.activePlayers.length > 1 ) {
-                    // if(hand.war) {
-                    //     call += this.createCall("#warOver#", this.players[winner], this.players[loser], hand.play[winner].identifier, hand.play[loser].identifier, hand.prize);
-                    // } 
-                    if(streak > 1) {
-                        call += this.createCall("#again#", this.players[winner], this.players[loser], hand.play[winner].identifier, hand.play[loser].identifier, hand.prize);
-                    } else {
-                        call += this.createCall("#call#", this.players[winner], this.players[loser], hand.play[winner].identifier, hand.play[loser].identifier, hand.prize);
-                    }
-                    
-                    const spread = hand.counts[0] - hand.counts[1];
-
-                    if(Math.abs(spread - spreadAtLastUpdate) >= 8) {
-                        if(hand.counts[0] !== hand.counts[1]) {
-                            
-                            call += ` ${this.players[leader].lastName} leads ${hand.counts[leader]} to ${hand.counts[(leader + 1) % 2]}.`;
-                        } else {
-                            call += " The games tied.";
-                        }
-                        spreadAtLastUpdate = spread;
-                    }
-                } else {
-                    call += `That's the match folks. ${this.players[loser].fullName} has run out of cards.`;
-                }
-                lastLeader = leader;
-            } else {
-                let rank = this.deck.getRank(hand.play[0].identifier);
-                streak = 0;
-                
-                call += this.grammar.flatten(`[card:${rank.toLowerCase()}]#tied#`);
-            }
-            wordCount += call.split(" ").length
-            calls.push(call);
-        }
         call = `${this.winner} wins in ${this.hands.length < 150 ? "just " : ""}${this.hands.length} hands.`;
         calls.push(call);
         return calls;
