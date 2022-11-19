c25803e94491a0ac451ccc7b8aecb032c27db132
Kelly Egan <kelly.egan@gmail.com>
2022-11-19 14:05:16 -0500

    Add space for color commentator in commentary text.

--- GameDirector.js
+++ GameDirector.js
@@ -11,13 +11,13 @@ class GameDirector {
     this.pbp = new PlayByPlay(this.game, deck);
   }
 
-  
   getCommentary() {
     let text = [];
     text.push(this.pbp.getIntro());
 
     for( let comment of this.pbp.getCall()) {
       text.push(`**${this.announcers.mainCommentator.lastName.toUpperCase()}:** ${comment}`);
+      text.push(`**${this.announcers.colorCommentator.lastName.toUpperCase()}:** So many colors!`);
     }
 
     text.push(this.pbp.getConclusion());
